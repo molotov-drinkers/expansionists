b86788e072187cfa2ba9f008fc816c002383c91e
Lucas Alves <hello@luhalvesbr.dev>
2024-12-19 13:17:27 -0300

    adding reminders for later

--- codebase/rust/src/globe/globe.rs
+++ codebase/rust/src/globe/globe.rs
@@ -41,39 +41,7 @@ impl INode3D for GlobeScene {
         &territory_data.unwrap().location.continent
       );
 
-      // TODO: Remove all this commented code
-      // if territory_name == "atlantic_forest".into() || territory_name == "latin_variations".into() {
-      //   color = Color::WHITE_SMOKE;
-      // }
-
-      // if territory_name == "pampas".into() || territory_name == "the_islands".into() {
-      //   color = Color::LIGHT_PINK;
-      // }
-
-      // if territory_name == "tropical_highlands".into() {
-      //   color = Color::LIGHT_YELLOW;
-      // }
-
-      // if territory_name == "caatinga".into() || territory_name == "west_slavs".into() {
-      //   color = Color::LIGHT_SALMON;
-      // }
-      
-      // if territory_name == "amazon".into() || territory_name == "rhine_region".into() {
-      //   color = Color::LIGHT_CYAN;
-      // }
-
-      // if territory_name == "andes".into() || territory_name == "nordics".into() {
-      //   color = Color::LIGHT_SLATE_GRAY;
-      // }
-
-      // if territory_name == "incas".into() || territory_name == "balkan_peninsula".into() {
-      //   color = Color::LIGHT_CYAN;
-      // }
-
-      // if territory_name == "patagonia".into() || territory_name == "north_black_sea".into()  {
-      //   color = Color::LIGHT_STEEL_BLUE;
-      // }
-
+      // TODO: Remove this condition once we have all highlithing territories features
       if territory_name == "patagonia".into() || territory_name == "antartica_peninsula".into() {
         color = Color::LIGHT_CYAN;
       }
@@ -82,23 +50,22 @@ impl INode3D for GlobeScene {
       material.set_albedo(color);
       territory.set_material_override(&material);
 
+      // let collision_shape_territory = territory
+      //   .find_child("StaticBody3D")
+      //   .expect("StaticBody3D to exist")
+      //   .find_child("CollisionShape3D")
+      //   .expect("CollisionShape3D to exist");
 
-      let collision_shape_territory = territory
-        .find_child("StaticBody3D")
-        .expect("StaticBody3D to exist")
-        .find_child("CollisionShape3D")
-        .expect("CollisionShape3D to exist");
+      // let collision_shape_territory = collision_shape_territory.cast::<CollisionShape3D>();
 
-      let collision_shape_territory = collision_shape_territory.cast::<CollisionShape3D>();
-
-      let _territory_position = collision_shape_territory.get_global_transform();
+      // let _territory_position = collision_shape_territory.get_global_transform();
       // godot_print!("Territory: {:?}, position: {:?}", territory_name, territory_position);
-
-
     }
   }
 
   fn physics_process(&mut self, _delta: f64) {
 
+    // TICKET: #49
+    // Should catch a event and highlight the territory by changing its color a by a color slightly ligheter
   }
 }
