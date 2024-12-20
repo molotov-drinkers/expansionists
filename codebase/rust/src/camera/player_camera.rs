411ab7f4a7fcb118a8780bae1e6bb2de41f43081
Lucas Alves <hello@luhalvesbr.dev>
2024-12-20 00:06:36 -0300

    ticket #31 solved and done

--- codebase/rust/src/camera/player_camera.rs
+++ codebase/rust/src/camera/player_camera.rs
@@ -1,4 +1,5 @@
-use godot::classes::{Camera3D, ICamera3D};
+use godot::classes::{Camera3D, ICamera3D, InputEvent, InputEventMouseButton};
+use godot::global::MouseButton;
 use godot::prelude::*;
 
 const MAX_HEIGHT_ANGLE: f32 = 89.9;
@@ -27,20 +28,16 @@ impl ICamera3D for PlayerCamera {
       base,
       // TICKET: #44
       camera_speed: 4.,
-      zoom_speed: 4.,
+      zoom_speed: 0.1,
       theta: 0.0,
       phi: 0.0,
     }
   }
 
   fn process(&mut self, delta: f64) {
-    let mut transform = self.base().get_global_transform();
     let input = Input::singleton();
 
-    let cam_location = self.base().get_global_position();
-    let world_origin = Vector3::new(0.0, 0.0, 0.0);
-    let mut radius = cam_location.distance_to(world_origin);
-    let vector_to_origin = (world_origin - cam_location).normalized();
+    let (radius, vector_to_origin) = self.get_data_to_move_camera();
 
     if input.is_action_pressed("camera_up") {
       self.phi = (self.phi + self.camera_speed as f32 * delta as f32).clamp(
@@ -61,16 +58,44 @@ impl ICamera3D for PlayerCamera {
       self.theta -= (self.camera_speed * delta) as f32;
     }
 
-    // Adjust radius (Zoom In/Out)
-    if input.is_action_pressed("zoom_in") {
-      radius += (self.zoom_speed * delta) as f32;
+    self.set_new_position(radius, vector_to_origin);
+  }
+
+  fn input(&mut self, event: Gd<InputEvent>) {
+    if let Ok(mouse_click) = event.try_cast::<InputEventMouseButton>() {
+      let mouse_button = mouse_click.get_button_index();
+
+      let (mut radius, vector_to_origin) = self.get_data_to_move_camera();
+
+      match mouse_button {
+        MouseButton::WHEEL_DOWN => {
+          radius += self.zoom_speed as f32
+        },
+        MouseButton::WHEEL_UP => {
+          radius -= self.zoom_speed as f32
+        },
+        _ => {}
+      }
+
+      self.set_new_position(radius, vector_to_origin);
     }
-    if input.is_action_pressed("zoom_out") {
-      radius -= (self.zoom_speed * delta) as f32;
-    }
+  }
+}
+
+impl PlayerCamera {
+  fn get_data_to_move_camera(&mut self) -> (f32, Vector3) {
+    let cam_location = self.base().get_global_position();
+    let world_origin = Vector3::new(0.0, 0.0, 0.0);
+    let vector_to_origin = (world_origin - cam_location).normalized();
+    let radius = cam_location.distance_to(world_origin);
+
+    (radius, vector_to_origin)
+  }
+
+  fn set_new_position(&mut self, mut radius: f32, vector_to_origin: Vector3) {
+    let mut transform = self.base().get_global_transform();
 
     radius = radius.clamp(MIN_DISTANCE_TO_ORIGIN, MAX_DISTANCE_TO_ORIGIN);
-
     transform.origin = Vector3::new(
       radius * self.phi.cos() * self.theta.cos(),
       radius * self.phi.sin(),
